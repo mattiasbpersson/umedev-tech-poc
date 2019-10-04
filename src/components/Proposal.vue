dc1797535062f55cb7e08917d695b65295e31174
mattiasbpersson <mattiasbpersson@gmail.com>
2019-10-04 19:12:06 +0200

    First commit

---
+++ src/components/Proposal.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            v-model="proposal.name"
+            :rules="fieldRules"
+            type="text"
+            label="Presenter(s)"
+            outlined
+          ></v-text-field>
+
+          <v-text-field
+            v-model="proposal.mail"
+            :rules="emailRules"
+            type="email"
+            label="Contact email"
+            outlined
+          ></v-text-field>
+
+          <v-text-field
+            v-model="proposal.title"
+            :rules="fieldRules"
+            type="text"
+            label="Title of presentation"
+            outlined
+          ></v-text-field>
+
+          <v-textarea
+            outlined
+            v-model="proposal.description"
+            :rules="descriptionRules"
+            type="text"
+            name="description"
+            label="Description of presentation"
+          ></v-textarea>
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="submit"
+          >
+            Submit
+          </v-btn>
+
+          <v-btn color="error" class="mr-4" @click="reset">
+            Reset Form
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { db } from '../db'
+var proposals_db = db.collection('proposals')
+
+export default Vue.extend({
+  data: () => ({
+    proposals: [],
+    valid: true,
+    proposal: {
+      name: "",
+      title: "",
+      mail: "",
+      description: "",
+    },
+    fieldRules: [
+      v => !!v || "Field is required",
+      v => (v && v.length <= 100) || "Field must be less than 100 characters"
+    ],
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v =>
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        ) || "E-mail must be valid"
+    ],
+    descriptionRules: [
+      v => !!v || "Description is required",
+      v =>
+        (v && v.length <= 1000) ||
+        "Description must be less than 1000 characters"
+    ],
+  }),
+  firestore: {
+    proposals: proposals_db,
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        //console.log("Submit:" + this.proposal);
+        proposals_db.add(this.proposal)
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    }
+  }
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import { firestorePlugin } from 'vuefire'
 
-Vue.config.productionTip = false
+Vue.use(firestorePlugin)
+
+Vue.config.productionTip = false;
+//Vue.use(firestorePlugin)
+
+  // Your web app's Firebase configuration
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyCBGb1e6vB1DOwi6KvbpH8lwRb0N7R6hpE",
+  //   authDomain: "umedev-2edfe.firebaseapp.com",
+  //   databaseURL: "https://umedev-2edfe.firebaseio.com",
+  //   projectId: "umedev-2edfe",
+  //   storageBucket: "",
+  //   messagingSenderId: "921144031224",
+  //   appId: "1:921144031224:web:5fae69164f53ef4d84c80d",
+  //   measurementId: "G-ZKMBEJEM3X"
+  // };
+  // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
