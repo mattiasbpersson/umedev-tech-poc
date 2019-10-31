e1fe166c941d59e11a0ab8c92bf9f86a0a40548b
Mattias Persson <mattiasbpersson@gmail.com>
2019-10-31 14:15:03 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,22 +6,6 @@ import { rtdbPlugin } from 'vuefire'
 Vue.use(rtdbPlugin)
 
 Vue.config.productionTip = false;
-//Vue.use(firestorePlugin)
-
-  // Your web app's Firebase configuration
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyCBGb1e6vB1DOwi6KvbpH8lwRb0N7R6hpE",
-  //   authDomain: "umedev-2edfe.firebaseapp.com",
-  //   databaseURL: "https://umedev-2edfe.firebaseio.com",
-  //   projectId: "umedev-2edfe",
-  //   storageBucket: "",
-  //   messagingSenderId: "921144031224",
-  //   appId: "1:921144031224:web:5fae69164f53ef4d84c80d",
-  //   measurementId: "G-ZKMBEJEM3X"
-  // };
-  // Initialize Firebase
-  //firebase.initializeApp(firebaseConfig);
-  //firebase.analytics();
 
 new Vue({
   vuetify,
